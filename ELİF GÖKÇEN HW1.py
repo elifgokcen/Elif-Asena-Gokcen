36056ffabfd30ddfbac7b2adf97be95ebbc0916c
elifgokcen <47911733+elifgokcen@users.noreply.github.com>
2019-03-03 14:12:44 +0300

    Update ELİF GÖKÇEN HW1.py

--- ELİF GÖKÇEN HW1.py
+++ ELİF GÖKÇEN HW1.py
@@ -1,9 +1,3 @@
-echo "# Elif-Asena-Gokcen-HW-1" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/elifgokcen/Elif-Asena-Gokcen-HW-1.git
-git push -u origin master
 
 import random 
 import datetime
